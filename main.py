7f70e31deb3242bb98faa01cc742347a20e352df
Vitaly <v.izhakivskiy@gmail.com>
2023-01-02 00:54:04 +0200

    checker

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import argparse
+import asyncio
+import socket
 
+import aiohttp as aiohttp
+from mcstatus import JavaServer
 from sqlalchemy import create_engine
 
 import network_scanner
@@ -12,6 +16,8 @@ from network_scanner.port_checker import PortCheckResult
 network_scanner.db_manager.setup_database(
     create_engine("postgresql://postgres:postgres@localhost/IPDB")
 )
+
+out_filename = 'out.txt'
 
 
 def parse_ports(ports):
@@ -29,13 +35,102 @@ def ip_lookup(ip_addr):
 
 
 def range_lookup(ip_addr):
-    return IPRange.lookup_ip_addr(ip_addr)
+    return IPRange.lookup(ip_addr)
 
 
 def on_port_opened(res: PortCheckResult):
+    global out_filename
     print(f'{res.ip}:{res.port}')
-    with open('out.txt', 'a') as out:
+    with open(out_filename, 'a') as out:
         out.write(f'{res.ip}:{res.port}\n')
+
+
+async def send_discord(json_):
+    discord_hook = 'https://discord.com/api/webhooks/1044305547358380065/' \
+                   'AH8dajJBb8yZA7dfGKkjbEFkbtzwC3blVqKwJ1jRF2fiPouyZjJIoAT13ko-LYWiDU1q'
+    async with aiohttp.ClientSession() as session:
+        await session.post(discord_hook, json=json_)
+
+
+def checker(filename):
+    loop = asyncio.get_event_loop()
+    with open(filename, 'r', encoding='utf-8') as ip_list:
+        for addr in ip_list:
+            try:
+                status = JavaServer.lookup(addr).status()
+                ip, port = addr.split(':')
+                ip_info = IPAddr.lookup(ip)
+                loop.run_until_complete(send_discord(
+                    {
+                        "content": None,
+                        "embeds": [
+                            {
+                                "color": None,
+                                "fields": [
+                                    {
+                                        "name": "IP address",
+                                        "value": ip,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Port",
+                                        "value": port,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "IP location",
+                                        "value": ip_info.country,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Region",
+                                        "value": ip_info.region,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "City",
+                                        "value": ip_info.city,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Latitude",
+                                        "value": str(ip_info.latitude),
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Longitude",
+                                        "value": str(ip_info.longitude),
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "MOTD",
+                                        "value": status.description,
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Online",
+                                        "value": f'{status.players.online}/{status.players.max}',
+                                        "inline": True
+                                    },
+                                    {
+                                        "name": "Version",
+                                        "value": str(status.version.name),
+                                    }
+                                ],
+                                "author": {
+                                    "name": "Minecraft server scanner",
+                                    "url": "https://pornhub.com/",
+                                    "icon_url": "https://play-lh.googleusercontent.com/VSwHQjcAttxsLE47RuS4PqpC4LT7lCoSjE7Hx5AW_yCxtDvcnsHHvm5CTuL5BPN-uRTP"
+                                }
+                            }
+                        ],
+                        "attachments": []
+                    }
+                ))
+                print(status.description, status.version.name, f'{status.players.online}/{status.players.max}',
+                      ip_info.country)
+            except (socket.timeout, BrokenPipeError, OSError):
+                pass
 
 
 if __name__ == "__main__":
@@ -56,6 +151,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     if args.mode == 'scanner':
+        out_filename = args.file
         country_ips = IPRange.fetch_all_by_country_code(args.country)
         with WorkManager() as manager:
             for i, ips_chunks in enumerate(utils.chunks(country_ips, len(country_ips) // args.workers)):
@@ -69,3 +165,5 @@ if __name__ == "__main__":
     elif args.mode == 'db-init':
         network_scanner.db_manager.create_all()
         import_from_csv(args.file)
+    elif args.mode == 'checker':
+        checker(args.file)
